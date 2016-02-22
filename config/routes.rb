30567746a4c227fde88c8d188d0b786a1e08565e
pablogfb <pablobuntu@gmail.com>
2016-02-22 13:21:23 +0100

    Modificaciones devise

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   get 'resource/update'
 
   devise_for :users
-root to: "home#index"
+root to: "resource#index"
+
+
  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222073710) do
+ActiveRecord::Schema.define(version: 20160222084822) do
+
+  create_table "resources", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "author",      limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "views",       limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "userName",               limit: 255
